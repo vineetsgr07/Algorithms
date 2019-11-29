4c0c9525874a815089f9a7104be1309758226260
vineet <vineet.sgr07@gmail.com>
2019-11-29 17:41:19 +0530

    uodated solution

--- Linked List/2.4-Partition.js
+++ Linked List/2.4-Partition.js
@@ -1,40 +1,32 @@
+/**
+ * Partition: Write code to partition a linked list around a value x, such that all nodes less than x come
+before all nodes greater than or equal to x. If x is contained within the list, the values of x only need
+to be after the elements less than x (see below). The partition element x can appear anywhere in the
+"right partition"; it does not need to appear between the left and right partitions.
+ */
+
 class LinkedListNode {
-    constructor(data) {
-      if (data === null) {
-        this.next = null;
-      } else {
-        this.data = data;
-        this.next = null;
-      }
+  constructor(data) {
+    if (data === null) {
+      this.next = null;
+    } else {
+      this.data = data;
+      this.next = null;
     }
   }
-  
-  const head = new LinkedListNode();
-  const first = new LinkedListNode("a");
-  const second = new LinkedListNode("b");
-  const thrid = new LinkedListNode("c");
-  const fourth = new LinkedListNode("d");
-  const fifth = new LinkedListNode("e");
-  const sixth = new LinkedListNode("f");
-  
-  head.next = first;
-  first.next = second;
-  second.next = thrid;
-  thrid.next = fourth;
-  fourth.next = fifth;
-  fifth.next = sixth;
-  
-  let kthElement = "d";
-  let kthElementPointer;
-  let curr = first;
-  
-  while (curr !== null) {
-    if (curr.data === kthElement) {
-      kthElementPointer = curr;
-      break;
-    }
-    curr = curr.next;
-  }
-  
-  console.log(kthElementPointer);
-  
+}
+
+const head = new LinkedListNode();
+const first = new LinkedListNode("a");
+const second = new LinkedListNode("b");
+const thrid = new LinkedListNode("c");
+const fourth = new LinkedListNode("d");
+const fifth = new LinkedListNode("e");
+const sixth = new LinkedListNode("f");
+
+head.next = first;
+first.next = second;
+second.next = thrid;
+thrid.next = fourth;
+fourth.next = fifth;
+fifth.next = sixth;
--- Linked List/2.6-Palindrome.js
+++ Linked List/2.6-Palindrome.js
@@ -1,40 +1,61 @@
+/**
+ * Palindrome: Implement a function to check if a linked list is
+ * a palindrome.
+ */
+
 class LinkedListNode {
-    constructor(data) {
-      if (data === null) {
-        this.next = null;
-      } else {
-        this.data = data;
-        this.next = null;
-      }
+  constructor(data) {
+    if (data === null) {
+      this.next = null;
+    } else {
+      this.data = data;
+      this.next = null;
     }
   }
-  
-  const head = new LinkedListNode();
-  const first = new LinkedListNode("a");
-  const second = new LinkedListNode("b");
-  const thrid = new LinkedListNode("c");
-  const fourth = new LinkedListNode("d");
-  const fifth = new LinkedListNode("e");
-  const sixth = new LinkedListNode("f");
-  
-  head.next = first;
-  first.next = second;
-  second.next = thrid;
-  thrid.next = fourth;
-  fourth.next = fifth;
-  fifth.next = sixth;
-  
-  let kthElement = "d";
-  let kthElementPointer;
-  let curr = first;
-  
-  while (curr !== null) {
-    if (curr.data === kthElement) {
-      kthElementPointer = curr;
-      break;
-    }
-    curr = curr.next;
+}
+
+const headP1 = new LinkedListNode();
+const first = new LinkedListNode(1);
+const second = new LinkedListNode(2);
+const thrid = new LinkedListNode(3);
+const fourth = new LinkedListNode(2);
+const fifth = new LinkedListNode(1);
+
+headP1.next = first;
+first.next = second;
+second.next = thrid;
+thrid.next = fourth;
+fourth.next = fifth;
+
+let curr = first;
+let p1 = curr;
+let p2;
+let stack = [];
+
+while (p1) {
+  stack.push(p1.data);
+  if (p2 && p2.next === null) {
+    break;
+  } else {
+    p1 = p1.next;
+    if (p1.next !== null) p2 = p1.next.next;
   }
-  
-  console.log(kthElementPointer);
-  
+}
+
+let isPalindrome = true;
+
+while (p1) {
+  let popElement = stack.pop();
+  if (popElement === p1.data) {
+    p1 = p1.next;
+  } else {
+    isPalindrome = false;
+    break;
+  }
+}
+
+if (isPalindrome) {
+  console.log("Sequence us Palindrome");
+} else {
+  console.log("Not Palindrome");
+}
